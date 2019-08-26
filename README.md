42f841dadd6240705247041c542eeb56d924d919
Hugo Matalonga <hmatalonga@gmail.com>
2019-08-26 14:39:33 +0100

    fix: install mkdir command flag

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dataset-converter-addons
+
+```shell
+$ curl -fsSL https://raw.githubusercontent.com/greenhub-project/dataset-converter-addons/master/install.sh | sh
+```
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Installing addons..."
-mkdir .tmp && cd .tmp
+mkdir -p .tmp && cd .tmp
 curl -sSOL https://github.com/greenhub-project/dataset-converter-addons/archive/master.zip
 unzip -oqj master.zip "dataset-converter-addons-master/data/*" -d "$(dirname $PWD)/data"
 unzip -oqj master.zip "dataset-converter-addons-master/plugins/*" -d "$(dirname $PWD)/plugins"
