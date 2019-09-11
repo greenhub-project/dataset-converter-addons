4172042cbd572231598fc786af09516d6ba51a4c
Hugo Matalonga <hmatalonga@gmail.com>
2019-09-11 19:49:47 +0100

    feat: change config default folder

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Installing addons..."
 mkdir -p .tmp && cd .tmp
 curl -sSOL https://github.com/greenhub-project/dataset-converter-addons/archive/master.zip
-unzip -oqj master.zip "dataset-converter-addons-master/data/*" -d "$(dirname $PWD)/data"
+unzip -oqj master.zip "dataset-converter-addons-master/config/*" -d "$(dirname $PWD)/config"
 unzip -oqj master.zip "dataset-converter-addons-master/plugins/*" -d "$(dirname $PWD)/plugins"
 cd .. && rm -rf .tmp
 echo "Done!"
